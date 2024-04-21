10ef297be0a7a6237c33144bea5f6f8850440463
masfazan <msfazan@icloud.com>
2024-04-21 16:09:38 -0300

    Add project files.

---
+++ triangulos3/Program.cs
@@ -0,0 +1,32 @@
+﻿float lado1=0, lado2=0, lado3=0;
+
+Console.WriteLine("Digite o comprimento dos três lados do triângulo: ");
+
+Console.Write("Digite o lado 1: ");
+lado1= float.Parse(Console.ReadLine());
+
+Console.Write("Digite o lado 2: ");
+lado2 = float.Parse(Console.ReadLine());
+
+Console.Write("Digite o lado 3: ");
+lado3 = float.Parse(Console.ReadLine());
+
+if (lado1 + lado2 > lado3 && lado2 + lado3 > lado1 && lado1 + lado3 > lado2)
+{
+    if (lado1 == lado2 && lado2 == lado3)
+    {
+        Console.WriteLine("O triângulo é equilátero");
+    }
+    else if (lado1 == lado2 || lado2 == lado3 || lado3 == lado1)
+    {
+        Console.WriteLine("O triângulo é isósceles");
+    }
+    else
+    {
+        Console.WriteLine("O triângulo é escaleno");
+    }
+}
+else
+{
+    Console.WriteLine("As medidas não são válidas.");
+}
